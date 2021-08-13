d0a823625abba1b21355f4648684c24132916aa4
Vitor Rubim <vitorrubim1@hotmail.com>
2021-08-13 18:58:55 -0300

    feat: using custom fonts with expo and making the status bar part of app.tsx for every application

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StatusBar } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import IllustrationImage from '../../assets/illustration.png';
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -9,23 +9,17 @@ import { styles } from './styles';
 export const SignIn: React.FC = () => {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-
       <Image source={IllustrationImage} style={styles.image} resizeMode="stretch" />
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
 
