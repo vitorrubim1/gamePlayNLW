edad3ad382696d0c69089f758259a81e82f7df30
Vitor Rubim <vitorrubim1@hotmail.com>
2021-08-19 15:31:42 -0300

    feat: creating component for add button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native'
 
-import Profile from '../../components/Profile';
+import { Profile } from '../../components/Profile';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { Background } from '../../components/Background';
+import { CategorySelect } from '../../components/CategorySelect';
 
 import { styles } from './styles';
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Profile />
-      </View>
+  const [category, setCategory] = useState('category');
 
-    </View>
+  function handleSelectCategory(categoryId: string) {
+    if (categoryId === category) {
+      setCategory('');
+    } else {
+      setCategory(categoryId);
+    }
+  }
+
+  return (
+    <Background>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Profile />
+          <ButtonAdd />
+        </View>
+
+        <CategorySelect categorySelected={category} setCategory={handleSelectCategory} />
+      </View>
+    </Background>
   );
 }
