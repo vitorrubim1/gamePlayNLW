97c1adb677907d4248853d41290e9824df9d6ca2
Vitor Rubim <vitorrubim1@hotmail.com>
2021-08-30 18:07:37 -0300

    feat: creating button, textArea and smallInput componentes

---
+++ src/components/SmallInput/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: 48,
+    height: 48,
+
+    backgroundColor: theme.colors.secondary40,
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.text400,
+    fontSize: 13,
+    textAlign: 'center',
+
+    borderRadius: 8,
+
+    marginRight: 4
+  },
+})
