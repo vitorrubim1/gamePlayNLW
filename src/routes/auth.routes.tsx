8acd8e1c346b1a6da7e27501134fbf93de80ed4f
Vitor Rubim <vitorrubim1@hotmail.com>
2021-09-01 21:53:16 -0300

    feat: separating public and private routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { Home } from "../screens/Home";
-import { SignIn } from "../screens/SignIn";
 import { AppointmentCreate } from "../screens/AppointmentCreate";
 import { AppointmentDetails } from "../screens/AppointmentDetails";
 
@@ -19,7 +18,6 @@ export function AuthRoutes() {
         }
       }}
     >
-      <Screen name="SignIn" component={SignIn} />
       <Screen name="Home" component={Home} />
       <Screen name="AppointmentCreate" component={AppointmentCreate} />
       <Screen name="AppointmentDetails" component={AppointmentDetails} />
